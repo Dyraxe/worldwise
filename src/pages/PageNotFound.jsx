4b7af4172c5b40acb0c402a4a31d8e40d565cb82
Paulo Amorim <phdja25@gmail.com>
2023-09-17 19:16:03 -0300

    Build base navigation for pages.

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>ERROR</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>ERROR</h1>
+    </div>
+  );
 }
 
 export default PageNotFound;
